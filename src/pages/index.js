dba79425708783eb636b150d76a4bbc0bed317a6
Karera-o <olivierkarera2020@gmail.com>
2022-12-26 17:26:42 +0200

    initializing the project

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import * as React from "react"
+
+export default function Home() {
+  return <div>Hello world!
+    <p>Proident qui proident et commodo nostrud incididunt sit incididunt ullamco irure.</p>
+  </div>
+}
