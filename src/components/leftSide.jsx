d1635eaad7284978f275900afb1ffcc5ec3dc15d
Karera-o <olivierkarera2020@gmail.com>
2022-12-26 18:02:40 +0200

    wip

---
+++ src/components/leftSide.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const LeftSide = () => {
+  return (
+    <div>
+        <div>
+            <span></span>
+            <p className='uppercase'>eres</p>
+        </div>
+        <div>
+            <h1>
+                <span></span>
+                Dashboard
+                <span></span>
+            </h1>
+            <ul>
+                <li>Dashboard Light</li>
+            </ul>
+        </div>
+        <div>
+            <h1>
+                <span></span>
+                Appointment
+                </h1>
+                <ul>
+                    <li><span></span> Staff <span></span></li>
+                </ul>
+        </div>
+
+        <div>
+            <h1><span></span> Widget</h1>
+            <ul>
+                <li><span></span> Forms</li>
+            </ul>
+        </div>
+
+        <div>
+            <p>Check your job schedule</p>
+            <span></span>
+        </div>
+
+    </div>
+  )
+}
+
+export default LeftSide
