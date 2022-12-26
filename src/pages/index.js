8d4783c3048deaf41369e3d09b03682eae5e7cb5
Karera-o <olivierkarera2020@gmail.com>
2022-12-26 17:41:16 +0200

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 export default function Home() {
-  return <div>Hello world!
-    <p>Proident qui proident et commodo nostrud incididunt sit incididunt ullamco irure.</p>
+  return <div className="bg-blue-500">Hello world!
+    <p className="bg-yellow-400">Proident qui proident et commodo nostrud incididunt sit incididunt ullamco irure.</p>
   </div>
 }
